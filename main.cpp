157c1c65e14a10fad3836cdf97024b33750df04b
Fábio Neto <fabiogvdneto@gmail.com>
2023-01-03 19:38:03 +0000

    Updated source code: better organization and comments.

--- main.cpp
+++ main.cpp
@@ -13,15 +13,25 @@ class DisjointSet {
         int *parents;
         int *ranks;
 
+        /**
+         * Links 
+         */
+        void Link(int u, int v) {
+            if (ranks[u] > ranks[v]) {
+                parents[v] = u;
+            } else {
+                parents[u] = v;
+
+                if (ranks[u] == ranks[v]) {
+                    ranks[v]++;
+                }
+            }
+        }
+
     public:
         DisjointSet(int size) {
             parents = new int[size];
             ranks = new int[size];
-
-            for (int u = 0; (u < size); u++) {
-                parents[u] = u;
-                ranks[u] = 0;
-            }
         }
 
         ~DisjointSet() {
@@ -32,26 +42,48 @@ class DisjointSet {
             ranks = nullptr;
         }
 
-        int find(int u) {
-            int x = parents[u];
-
-            return (x == u) ? x : (parents[u] = find(x));
+        /**
+         * Creates a new list with element u.
+         * 
+         * Time Complexity: O(1).
+         */
+        void MakeSet(int u) {
+            parents[u] = u;
+            ranks[u] = 0;
         }
 
-        void unite(int u, int v) {
-            link(find(u), find(v));
+        /**
+         * Returns pointer to the root of the subset where u belongs.
+         * 
+         * Uses Path Compression.
+         * 
+         * Time Complexity:
+         * - Worst Case: O(V).
+         * - Best Case: O(1).
+         * - Average: O(alpha(V)) - with path compression.
+         */
+        int FindSet(int u) {
+            return (u == parents[u]) ? u : (parents[u] = FindSet(parents[u]));
         }
 
-        void link(int x, int y) {
-            if (ranks[x] > ranks[y]) {
-                parents[y] = x;
-            } else {
-                parents[x] = y;
-
-                if (ranks[x] == ranks[y]) {
-                    ranks[y]++;
-                }
-            }
+        /**
+         * Performs the union of u and v: (finds the subsets of u and v, then links them).
+         * 1. Finds subset of u and v.
+         * 2. If subset is the same, returns false and has no side effect.
+         *    Otherwise, both subsets are linked.
+         * 
+         * Uses Union by Rank (when linking).
+         * 
+         * Time Complexity:
+         * - Worst Case: O(V).
+         * - Best Case: O(1).
+         * - Average: O(alpha(V)) - with union by rank.
+         */
+        bool Union(int u, int v) {
+            if ((u = FindSet(u)) == (v = FindSet(v))) return false;
+            
+            Link(u, v);
+            return true;
         }
 };
 
@@ -65,27 +97,35 @@ Edge *edges;
 
 /* Kruskal Algorithm */
 
-bool is_heavier(Edge e1, Edge e2) { return (e1.w > e2.w); }
-
-int kruskal() {
-    sort(edges, edges+n_edges, is_heavier);
+/**
+ * Applies Kruskal Algorithm to the graph to create a maximum spanning tree (MST).
+ * Returns the sum of the weights of the edges that are safe to the MST.
+ * 
+ * Time Complexity: O(E log(E)).
+ */
+int kruskal(void) {
+    // Initialization: O(E log(E)).
+    
+    sort(edges, edges+n_edges, [](const Edge& a, const Edge& b) {
+        return (a.w > b.w);
+    });
 
     DisjointSet set(n_vertices);
+
+    // MakeSet Operations: O(V).
+
+    for (int u = 0; (u < n_vertices); u++) { set.MakeSet(u); }
     
     int result = 0;
     int count = 0;
 
+    // Union (and FindSet) Operations: O(E alpha(V)).
+
     for (int i = 0; (i < n_edges); i++) {
-        Edge edge = edges[i];
+        if (!set.Union(edges[i].u, edges[i].v)) continue;
 
-        int x = set.find(edge.u);
-        int y = set.find(edge.v);
-
-        if (x == y) continue;
-
-        set.link(x, y);
-        result += edge.w;
-
+        result += edges[i].w; // i is a safe edge to the MST
+        
         if (++count == n_vertices-1) break;
     }
 
@@ -95,24 +135,26 @@ int kruskal() {
 /* Solver */
 
 int main(void) {
-    assert(scanf("%d%d", &n_vertices, &n_edges) == 2);
+    assert(scanf("%d %d", &n_vertices, &n_edges) == 2);
+    
     assert(n_vertices >= 1);
     assert(n_edges >= 0);
 
     edges = new Edge[n_edges];
 
+    // Scanning: O(E).
     for (int i = 0; (i < n_edges); i++) {
-        assert(scanf("%d", &edges[i].u) == 1);
+        assert(scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w) == 3);
+
         assert(edges[i].u > 0);
         assert(edges[i].u <= n_vertices);
 
-        assert(scanf("%d", &edges[i].v) == 1);
         assert(edges[i].v > 0);
         assert(edges[i].v <= n_vertices);
 
-        assert(scanf("%d", &edges[i].w) == 1);
         assert(edges[i].w >= 0);
 
+        // make it compatible with disjoint set indexing (starting at 0).
         edges[i].u--;
         edges[i].v--;
     }
